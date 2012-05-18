c490daee7759ac1e9301cf295031de5107dac03a
Danilo Kuehn <dk@nogo-software.de>
2012-05-18 16:12:31 +0200

    add first parser test

--- Tests/Controller/ParsersControllerTest.php
+++ Tests/Controller/ParsersControllerTest.php
@@ -7,7 +7,13 @@ class ParsersControllerTest extends DimeTestCase
     public function testProcess()
     {
         $response = $this->request('POST', '/api/process', '10:00-12:00 @cc/CWE2011:testing new magic oneline input');
-
         $this->assertEquals(200, $response->getStatusCode(), $response->getContent());
+        $content = json_decode($response->getContent(), true);
+
+        $this->assertEquals($content['description'], 'new magic oneline input');
+        $this->assertEquals($content['customer']['alias'], 'CC');
+        $this->assertEquals($content['project']['name'], 'CWE2011');
+        $this->assertEquals($content['service']['name'], 'testing');
+        $this->assertEquals($content['timeslices'][0]['duration'], 7200);
     }
 }
