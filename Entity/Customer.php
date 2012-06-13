4dbeeb960fed475e7d554ed19910131d7daf8f83
Danilo Kuehn <dk@nogo-software.de>
2012-06-13 18:09:02 +0200

    fix problem with index in sqlite database

--- Entity/Customer.php
+++ Entity/Customer.php
@@ -14,7 +14,7 @@ use JMS\SerializerBundle\Annotation\SerializedName;
  * @UniqueEntity(fields={"alias", "user"})
  * @ORM\Table(
  *   name="customers",
- *   uniqueConstraints={ @ORM\UniqueConstraint(columns={"alias", "user_id"}) }
+ *   uniqueConstraints={ @ORM\UniqueConstraint(name="unique_alias_user", columns={"alias", "user_id"}) }
  * )
  * @ORM\Entity(repositoryClass="Dime\TimetrackerBundle\Entity\CustomerRepository")
  */
