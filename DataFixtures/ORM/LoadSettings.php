fa3ddc9b1fff90420f6cb3e3deb240b6f2b90e21
Danilo Kuehn <dk@nogo-software.de>
2013-02-25 12:20:53 +0100

    remove settings fixtures

--- DataFixtures/ORM/LoadSettings.php
+++ DataFixtures/ORM/LoadSettings.php
@@ -15,14 +15,7 @@ class LoadSettings extends AbstractFixture implements OrderedFixtureInterface
      */
     public function load(ObjectManager $manager)
     {
-        $dayFilterSetting = new Setting();
-        $dayFilterSetting->setNamespace('activity-filter');
-        $dayFilterSetting->setName('date-period');
-        $dayFilterSetting->setValue('this-week');
-        $dayFilterSetting->setUser($manager->merge($this->getReference('default-user')));
-        $manager->persist($dayFilterSetting);
 
-        $manager->flush();
     }
 
     /**
